601c18fa243b00ea74396cb5f551e8b2589b8d06
Salman <rayhanartz@gmail.com>
2025-07-24 18:30:16 +0600

    String 3 Updates

--- src/StringsAll/String3.java
+++ src/StringsAll/String3.java
@@ -1,4 +1,38 @@
 package StringsAll;
 
 public class String3 {
+    public static void main(String[] args) {
+        String s1 = "Salman";
+        String s2 = " Farsi";
+
+        String fullname = s1+s2;
+        System.out.println("Full name: "+fullname);
+
+
+        printall();
+
+
+
+
+    }
+    static void upper(String s){
+        String Upper = s.toUpperCase();
+        System.out.println(Upper);
+    }
+
+    static void lower(String s){
+        String lower = s.toLowerCase();
+        System.out.println(lower);
+    }
+    static void startswith(String s){
+        boolean nom = s.startsWith("p");
+        System.out.println(nom);
+    }
+    static void printall(){
+        String[] s = {"Salman", "Shorif", "Shakil", "Jannat"};
+        for(String x: s){
+            System.out.println(x);
+        }
+
+    }
 }
