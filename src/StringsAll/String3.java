58ddba0bbca00ea6133a9dc06d9858a241b98b5e
Salman <rayhanartz@gmail.com>
2025-07-24 18:02:51 +0600

    String 3 creation

---
+++ src/StringsAll/String3.java
@@ -0,0 +1,4 @@
+package StringsAll;
+
+public class String3 {
+}
---
+++ src/StringsAll/Strings2.java
@@ -0,0 +1,18 @@
+package StringsAll;
+
+public class Strings2 {
+    public static void main(String[] args) {
+        String s1 = "Salman";
+        String s2 = "Salman";
+
+        int len1=s1.length();
+        int len2=s2.length();
+
+        if(len1==len2){
+            System.out.println("1");
+        }
+        else{
+            System.out.println("0");
+        }
+    }
+}
