32699c246b6e6981c847f4f92136dbbdee508cfd
Salman <rayhanartz@gmail.com>
2025-07-24 17:58:32 +0600

    Created Package and String update

---
+++ src/StringsAll/Strings.java
@@ -0,0 +1,15 @@
+package StringsAll;
+
+public class Strings {
+    public static void main(String[] args) {
+        String s1 = "Salman";
+        String s2 = "Salman";
+
+        if(s1.equals(s2)){
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("Not Equal");
+        }
+    }
+}
