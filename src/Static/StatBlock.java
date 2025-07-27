55b0e37120f2bfd85997708d99472e1487cc1902
Salman <rayhanartz@gmail.com>
2025-07-27 18:47:09 +0600

    Static Block Example

--- src/Static/StatBlock.java
+++ src/Static/StatBlock.java
@@ -9,7 +9,14 @@ public class StatBlock {
         name = "StatBlock Testing";
     }
 
-    void display1(){
+    static void display1(){
         System.out.println("ID:"+id+"\nName:"+name );
+    }
+
+    public static void main(String[] args) {
+        StatBlock.display1();
+        // WE dont have to reference and allocate memory for an object
+        // Because there is not need for object as we can access everything with class name
+        // This happened because of the static property.
     }
 }
