d55f037c1bf5119e04bc1b2112b277aea80bb1a5
Salman <rayhanartz@gmail.com>
2025-07-28 11:56:39 +0600

    Math class exploration

---
+++ src/MathClass/MathOpra.java
@@ -0,0 +1,23 @@
+package MathClass;
+
+public class MathOpra {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 9.5F;
+
+        //Absolute method
+        double x = Math.abs(-f);
+        System.out.println("Absolute value is : "+x);
+
+
+        //Square root value
+        double y = Math.sqrt(100);
+        System.out.println("Squared value is : "+y);
+
+        //power value
+        double z = Math.pow(3,2);
+        System.out.println("Power is : "+z);
+
+
+    }
+}
