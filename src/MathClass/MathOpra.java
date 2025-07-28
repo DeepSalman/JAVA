c8b79f394ddeb09d2470ba2eb95724d31393fe7f
Salman <rayhanartz@gmail.com>
2025-07-28 12:16:05 +0600

    Math class exploration

--- src/MathClass/MathOpra.java
+++ src/MathClass/MathOpra.java
@@ -18,6 +18,13 @@ public class MathOpra {
         double z = Math.pow(3,2);
         System.out.println("Power is : "+z);
 
+        System.out.println(Math.PI);
+        System.out.println(Math.log(2.0));
+        System.out.println(Math.exp(1.0));
+        System.out.println(Math.max(-2.3,12.8));
+        System.out.println(Math.ceil(-5.6));
+        System.out.println(Math.floor(-5.6));
+
 
     }
 }
