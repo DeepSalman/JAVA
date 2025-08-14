df00b211619a0ac4f82b6ff03de2b001163c5cdd
Salman <rayhanartz@gmail.com>
2025-08-15 00:04:25 +0600

    This Concept Basic

---
+++ src/DemoClass.java
@@ -0,0 +1,56 @@
+class Students1
+{
+    int id;
+    String name;
+    float cgpa;
+
+    {
+        System.out.println("Init Block");
+    }
+
+    Students1()
+    {
+        System.out.println("A new student is created.");
+    }
+
+    Students1(int id, String name)
+    {
+        this(); // Calls the Students1() constructor -> must always be first line
+        this.id = id;
+        this.name = name;
+        System.out.println("Constructor 2");
+        //this();
+    }
+
+    Students1(int id, String name, float cgpa)
+    {
+        this(id, name); // Calls the Students1(id, name) constructor
+        this.cgpa = cgpa;
+        System.out.println("Constructor 3");
+    }
+
+    Students1(Students1 s) // Creates a new Students1 object copying values from Students1 s
+    {
+        this.id = s.id;
+        this.name = s.name;
+        this.cgpa = s.cgpa;
+    }
+}
+
+
+public class DemoClass {
+
+    public static void main(String[] args) {
+
+        Students1 s1 = new Students1(10, "Bob");
+        System.out.println(s1.id + " " + s1.name);
+
+        Students1 s2 = new Students1(15, "Sam", 3.5f);
+
+        Students1 s3 = new Students1(s2);
+        System.out.println(s3.id + " " + s3.cgpa + " " + s3.name);
+
+
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-import java.sql.SQLOutput;
+public class Main{
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
 }
