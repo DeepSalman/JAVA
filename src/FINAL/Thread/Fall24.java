ae79d4b8fa711f6c5ad476e3cdcdd0110b9ddf69
Salman <rayhanartz@gmail.com>
2025-10-27 22:43:49 +0600

    Final Solve

--- src/FINAL/Thread/Fall24.java
+++ src/FINAL/Thread/Fall24.java
@@ -1,70 +1,46 @@
 package FINAL.Thread;
 
-class NegativePart extends Thread {
-    double sum = 0;
+class SeriesThread extends Thread
+{
     double x;
+    int start;
+    double sum = 0;
 
-    NegativePart(double x) {
+    SeriesThread(double x, int start)
+    {
         this.x = x;
+        this.start = start;
     }
 
-    public void run() {
-        // Negative terms are at even powers (x^1/2, x^3/4, x^5/6, ...)
-        for (int i = 1; i <= 50; i++) {
-            if (i % 2 == 1) { // odd power means negative term
-                sum -= Math.pow(x, i) / (i + 1);
-            }
+    public void run()
+    {
+        for(int i=start; i<=50; i += 2)
+        {
+            sum += Math.pow(x,i) / (i+1);
         }
-    }
-
-    public double getSum() {
-        return sum;
     }
 }
 
-class PositivePart extends Thread {
-    double sum = 0;
-    double x;
+class Code3 {
 
-    PositivePart(double x) {
-        this.x = x;
-    }
-
-    public void run() {
-        // Positive terms: 1, x^2/3, x^4/5, ...
-        sum += 1; // the first term
-        for (int i = 2; i <= 50; i++) {
-            if (i % 2 == 0) { // even power means positive term
-                sum += Math.pow(x, i) / (i + 1);
-            }
-        }
-    }
-
-    public double getSum() {
-        return sum;
-    }
-}
-
-class ZCalculator {
     public static void main(String[] args) {
-        double x = 1.15;
 
-        NegativePart negThread = new NegativePart(x);
-        PositivePart posThread = new PositivePart(x);
+        SeriesThread s1 = new SeriesThread(1.15, 0); // positive
+        SeriesThread s2 = new SeriesThread(1.15, 1); // negative
 
-        negThread.start();
-        posThread.start();
+        s1.start();
+        s2.start();
 
-        try {
-            negThread.join();
-            posThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        try
+        {
+            s1.join();
+            s2.join();
         }
+        catch(Exception e){}
 
-        double z = negThread.getSum() + posThread.getSum();
+        System.out.println("Sum of series = " + (s1.sum-s2.sum));
 
-        System.out.println("Final value of z = " + z);
+
     }
-}
 
+}
